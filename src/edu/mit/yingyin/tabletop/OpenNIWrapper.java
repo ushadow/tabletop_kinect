4e8866db1b57fa50f07a2c00f1bc67ff0d5d572c
Ying Yin <clevershadow@gmail.com>
2011-05-30 22:31:01 -0400

    Added more wrapper methods.

--- src/edu/mit/yingyin/tabletop/OpenNIWrapper.java
+++ src/edu/mit/yingyin/tabletop/OpenNIWrapper.java
@@ -5,26 +5,47 @@ import java.nio.IntBuffer;
 import rywang.util.DirectBufferUtils;
 
 public class OpenNIWrapper {
+  public static final int DEPTH_WIDTH = 640;
+  public static final int DEPTH_HEIGHT = 480;
+
   static { 
     System.loadLibrary("openni-java-wrapper"); 
   }
   
   public static void main(String[] args) {
     OpenNIWrapper openniWrapper = new OpenNIWrapper();
-    boolean ret = openniWrapper.initFromXmlFile("");
+    boolean ret = openniWrapper.initFromXmlFile("config/config.xml");
     System.out.println(ret);
-  
   }
   
-  private IntBuffer controlBlock;
+  private IntBuffer ctrlBlock;
+  private IntBuffer depthBuf;
   
   public OpenNIWrapper() {
-    controlBlock = DirectBufferUtils.allocateIntBuffer(2);
+    ctrlBlock = DirectBufferUtils.allocateIntBuffer(2);
+    depthBuf = DirectBufferUtils.allocateIntBuffer(DEPTH_HEIGHT * DEPTH_WIDTH);
   }
   
+  /**
+   * 
+   * @param configFile xml file name to initialize the OpenNI context.
+   * @return true if the initialization is successful; false otherwise.
+   */
   public boolean initFromXmlFile(String configFile) {
-    return initFromXmlFile(controlBlock, configFile);
+    return initFromXmlFile(ctrlBlock, configFile);
   }
   
-  private native boolean initFromXmlFile(IntBuffer cb, String configFile); 
+  public boolean waitAnyUpdateAll() {
+    return waitAnyUpdateAll(ctrlBlock);
+  }
+  
+  public void getDepthMap() {
+    getDepthMap(ctrlBlock, depthBuf, DEPTH_WIDTH, DEPTH_HEIGHT);
+  }
+  
+  private native boolean initFromXmlFile(IntBuffer ctrlBlock, 
+                                         String configFile); 
+  private native boolean waitAnyUpdateAll(IntBuffer ctrlBlock);
+  private native void getDepthMap(IntBuffer ctrlBlock, IntBuffer depthBuf,
+                                  int width, int height);
 }
