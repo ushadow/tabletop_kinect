c7ca2040ec7827e130ffbd5766fcf649f14eadbd
Ying Yin <clevershadow@gmail.com>
2011-05-31 21:06:47 -0400

    Added MainDriver.

---
+++ src/edu/mit/yingyin/tabletop/MainDriver.java
@@ -0,0 +1,54 @@
+package edu.mit.yingyin.tabletop;
+
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import edu.mit.yingyin.gui.ImageViewer;
+
+public class MainDriver {
+  public class Controller extends WindowAdapter {
+    public void windowClosing(WindowEvent e) {
+      running = false;
+    }
+  }
+  private OpenNIWrapper openni;
+  private ImageViewer viewer;
+  private boolean running = true;
+  
+  public MainDriver() {
+    openni = new OpenNIWrapper();
+    openni.initFromXmlFile("config/config.xml");
+    int depthWidth = openni.getDepthWidth();
+    int depthHeight = openni.getDepthHeight();
+    int[] depthMap = new int[depthWidth * depthHeight];
+    int index = 0, maxDepth = 1600;
+    BufferedImage image = new BufferedImage(depthWidth, depthHeight, 
+                                            BufferedImage.TYPE_INT_RGB);
+    viewer = new ImageViewer("Depth Map", 
+                             new Dimension(depthWidth, depthHeight));
+    viewer.addWindowListener(new Controller());
+    while (running) {
+      openni.waitAnyUpdateAll();
+      openni.getDepthMap(depthMap);
+      int[] dstArray = 
+          ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+      index = 0;
+      for (int h = 0; h < depthHeight; h++) {
+        for (int w = 0; w < depthWidth; w++, index++) 
+          dstArray[index] = (maxDepth - depthMap[index]) * 256 / 
+                            maxDepth & 0x000000ff;
+      }
+      viewer.show(image);
+    }
+    openni.cleanUp();
+    viewer.dispose();
+    System.exit(0);
+  }
+  
+  public static void main(String[] args) {
+    new MainDriver();
+  }
+}
--- src/edu/mit/yingyin/tabletop/OpenNIWrapper.java
+++ src/edu/mit/yingyin/tabletop/OpenNIWrapper.java
@@ -5,8 +5,8 @@ import java.nio.IntBuffer;
 import rywang.util.DirectBufferUtils;
 
 public class OpenNIWrapper {
-  public static final int DEPTH_WIDTH = 640;
-  public static final int DEPTH_HEIGHT = 480;
+  private static final int DEPTH_WIDTH = 640;
+  private static final int DEPTH_HEIGHT = 480;
 
   static { 
     System.loadLibrary("openni-java-wrapper"); 
@@ -15,7 +15,20 @@ public class OpenNIWrapper {
   public static void main(String[] args) {
     OpenNIWrapper openniWrapper = new OpenNIWrapper();
     boolean ret = openniWrapper.initFromXmlFile("config/config.xml");
-    System.out.println(ret);
+    System.out.println("initFromXmlFile: " + ret);
+    ret = openniWrapper.waitAnyUpdateAll();
+    System.out.println("waitAnyUpdateAll: " + ret);
+    int[] depthMap = 
+        new int[openniWrapper.getDepthWidth() * openniWrapper.getDepthHeight()];
+    openniWrapper.getDepthMap(depthMap);
+    int index = 0, maxDepth = 0;
+    for (int h = 0; h < openniWrapper.getDepthHeight(); h++) {
+      for (int w = 0; w < openniWrapper.getDepthWidth(); w++, index++)
+        maxDepth = maxDepth > depthMap[index] ? maxDepth : depthMap[index];
+        System.out.print(depthMap[index] + " ");
+      System.out.println();
+    }
+    openniWrapper.cleanUp();
   }
   
   private IntBuffer ctrlBlock;
@@ -26,6 +39,9 @@ public class OpenNIWrapper {
     depthBuf = DirectBufferUtils.allocateIntBuffer(DEPTH_HEIGHT * DEPTH_WIDTH);
   }
   
+  public int getDepthWidth() { return DEPTH_WIDTH; }
+  
+  public int getDepthHeight() { return DEPTH_HEIGHT; }
   /**
    * 
    * @param configFile xml file name to initialize the OpenNI context.
@@ -39,12 +55,19 @@ public class OpenNIWrapper {
     return waitAnyUpdateAll(ctrlBlock);
   }
   
-  public void getDepthMap() {
+  public void getDepthMap(int[] depthArray) {
     getDepthMap(ctrlBlock, depthBuf);
+    depthBuf.rewind();
+    depthBuf.get(depthArray);
+  }
+  
+  public void cleanUp() {
+    cleanUp(ctrlBlock);
   }
   
   private native boolean initFromXmlFile(IntBuffer ctrlBlock, 
                                          String configFile); 
   private native boolean waitAnyUpdateAll(IntBuffer ctrlBlock);
   private native void getDepthMap(IntBuffer ctrlBlock, IntBuffer depthBuf);
+  private native void cleanUp(IntBuffer ctrlBlock); 
 }
