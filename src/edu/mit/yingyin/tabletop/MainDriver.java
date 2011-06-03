43ffae290d30d0c0f1d263449b6068b60cb1b16d
Ying Yin <clevershadow@gmail.com>
2011-06-02 22:52:11 -0400

    Added find connected components method.

--- src/edu/mit/yingyin/tabletop/MainDriver.java
+++ src/edu/mit/yingyin/tabletop/MainDriver.java
@@ -5,8 +5,10 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
+import java.nio.ShortBuffer;
 
 import edu.mit.yingyin.gui.ImageViewer;
+import edu.mit.yingyin.tabletop.HandProcessor.DebugImage;
 
 public class MainDriver {
   private class Controller extends WindowAdapter {
@@ -19,15 +21,17 @@ public class MainDriver {
   private boolean running = true;
   
   public MainDriver() {
-    HandProcessor processor = new HandProcessor();
+    System.out.println("java.library.paht = " + 
+                       System.getProperty("java.library.path"));
     openni = new OpenNIWrapper();
     openni.initFromXmlFile("config/config.xml");
     int depthWidth = openni.getDepthWidth();
     int depthHeight = openni.getDepthHeight();
+    HandProcessor processor = new HandProcessor(depthWidth, depthHeight);
     int[] depthMap = new int[depthWidth * depthHeight];
-    int index = 0, maxDepth = 1600;
     BufferedImage image = new BufferedImage(depthWidth, depthHeight, 
                                             BufferedImage.TYPE_INT_RGB);
+    DebugImage debugImage = new DebugImage(depthWidth, depthHeight);
     viewer = new ImageViewer("Depth Map", 
                              new Dimension(depthWidth, depthHeight));
     viewer.addWindowListener(new Controller());
@@ -36,15 +40,15 @@ public class MainDriver {
       openni.waitAnyUpdateAll();
       openni.getDepthMap(depthMap);
       processor.processData(depthMap);
-      int[] dstArray = 
-          ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-      index = 0;
-      for (int h = 0; h < depthHeight; h++) {
-        for (int w = 0; w < depthWidth; w++, index++) 
-          dstArray[index] = depthMap[index] * 256 / 
-                            maxDepth & 0x000000ff;
-      }
-      viewer.show(image);
+//      int[] dstArray = 
+//          ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+//      index = 0;
+//      for (int h = 0; h < depthHeight; h++) {
+//        for (int w = 0; w < depthWidth; w++, index++) 
+//          dstArray[index] = (char)depthArray.get(index) * 256 / 
+//                            maxDepth & 0x000000ff;
+//      }
+      //viewer.show(debugImage.draw(processor.getContours()));
     }
     openni.cleanUp();
     viewer.dispose();
