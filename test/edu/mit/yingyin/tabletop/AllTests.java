ee822d87c649fcebab8ed04b14ec6faf012ae21e
yingyin <clevershadow@gmail.com>
2012-09-27 21:35:23 -0400

    Removed the test on jni implementation of OpenNI wrapper.

--- test/edu/mit/yingyin/tabletop/AllTests.java
+++ test/edu/mit/yingyin/tabletop/AllTests.java
@@ -9,11 +9,10 @@ import edu.mit.yingyin.tabletop.models.CalibrationExampleTest;
 import edu.mit.yingyin.tabletop.models.FullOpenNIDeviceTest;
 import edu.mit.yingyin.tabletop.models.HandAnalyzerTest;
 import edu.mit.yingyin.tabletop.models.HandTrackingEngineTest;
-import edu.mit.yingyin.tabletop.models.PartialOpenNIDeviceTest;
 import edu.mit.yingyin.tabletop.models.TableTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({FullOpenNIDeviceTest.class, PartialOpenNIDeviceTest.class,
+@SuiteClasses({FullOpenNIDeviceTest.class,
                TableTest.class, HandAnalyzerTest.class, EnvTest.class,
                CvUtilTest.class, BackgroundTest.class, 
                CalibrationExampleTest.class, JavaCVTest.class,
