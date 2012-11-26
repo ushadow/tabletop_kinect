479a184820f606ed77eff22dd2bac689bee94e85
yingyin <clevershadow@gmail.com>
2012-11-26 15:24:16 -0500

    Refactored the code for output.

--- src/edu/mit/yingyin/tabletop/apps/HandTrackingApp.java
+++ src/edu/mit/yingyin/tabletop/apps/HandTrackingApp.java
@@ -31,73 +31,74 @@ import edu.mit.yingyin.util.ObjectIO;
 /**
  * Application that tracks the fingertips in data from an OpenNI device. Saves
  * fingertip events to file in the end.
- *
+ * 
  * @author yingyin
- *
+ * 
  */
 public class HandTrackingApp extends KeyAdapter {
 
   /**
    * Listens to hand events.
+   * 
    * @author yingyin
-   *
+   * 
    */
   private class HandEventListener implements IHandEventListener {
     /**
      * List of finger events detected in a frame.
      */
-    private List<List<FingerEvent>> fingerEventList = 
-        new ArrayList<List<FingerEvent>>();
-    
+    private List<List<FingerEvent>> fingerEventList = new ArrayList<List<FingerEvent>>();
+
     @Override
     public void fingerPressed(List<FingerEvent> feList) {
       if (packetController != null) {
         for (FingerEvent fe : feList)
-          packetController.drawCircle((int)fe.posImage.x, (int)fe.posImage.y);
+          packetController.drawCircle((int) fe.posImage.x, (int) fe.posImage.y);
       }
       fingerEventList.add(feList);
     }
-    
+
+    /**
+     * Outputs finger events.
+     * 
+     * @param pw <code>PrinterWriter</code> for output.
+     */
     public void toOutput(PrintWriter pw) {
       pw.println("# frame-id x y z x y z ...");
       for (List<FingerEvent> list : fingerEventList) {
-        for (int i = 0; i < list.size(); i++) {
-          if (i == 0) {
-            pw.print(String.format("%d %d %d %d ", list.get(i).frameID, 
-                (int)list.get(i).posImage.x, (int)list.get(i).posImage.y, 
-                (int)list.get(i).posImage.z));
-          } else {
-            pw.print(String.format("%d %d %d ", (int)list.get(i).posImage.x, 
-                (int)list.get(i).posImage.y, (int)list.get(i).posImage.z));
-          }
+        if (list.isEmpty())
+          continue;
+        pw.print(list.get(0).frameID + " ");
+        for (FingerEvent fe : list) {
+          pw.print(String.format("%d %d %d ", (int) fe.posImage.x,
+              (int) fe.posImage.y, (int) fe.posImage.z));
         }
         pw.println();
       }
     }
   }
-  
-  private static Logger logger = Logger.getLogger(
-      HandTrackingApp.class.getName());
-  
-  private static final String CONFIG_FILE = 
-      "/config/fingertip_tracking.properties";
+
+  private static Logger logger = Logger.getLogger(HandTrackingApp.class.getName());
+
+  private static final String CONFIG_FILE = "/config/fingertip_tracking.properties";
   private static final String DATA_DIR = "/data/";
   private static final String FINGERTIP_DIR = DATA_DIR + "fingertip/";
   private static final String TIME_FORMAT = "yyyy-MM-dd_HH-mm-SS";
-  
+  private static final String OUTPUT_EXTENSION = ".log";
+
   @SuppressWarnings("static-access")
   public static void main(String[] args) {
-    Option mainDirOpt = OptionBuilder.withArgName("main directory").
-        withLongOpt("dir").
-        hasArg().withDescription("The main directory for input and output. " +
-        "The configuration file should be in <dir>/config/folder." + 
-        "The default dir is the current directory.").create("d");
+    Option mainDirOpt = OptionBuilder.withArgName("main directory").withLongOpt(
+        "dir").hasArg().withDescription(
+        "The main directory for input and output. "
+            + "The configuration file should be in <dir>/config/folder."
+            + "The default dir is the current directory.").create("d");
     CommandLineOptions.addOption(mainDirOpt);
     CommandLineOptions.parse(args);
     String mainDir = CommandLineOptions.getOptionValue("d", "./");
     new HandTrackingApp(mainDir);
   }
-  
+
   private HandTrackingEngine engine;
   private ProcessPacketController packetController;
   private HandEventListener handEventListener;
@@ -106,12 +107,12 @@ public class HandTrackingApp extends KeyAdapter {
   private boolean paused = false;
   private SimpleDateFormat dateFormat = new SimpleDateFormat(TIME_FORMAT);
   private Table3DFrame tableFrame;
-  
+
   @SuppressWarnings("unchecked")
-  public HandTrackingApp(String mainDir)  {
-    logger.info("java.library.path = " + 
-        System.getProperty("java.library.path"));
-    
+  public HandTrackingApp(String mainDir) {
+    logger.info("java.library.path = "
+        + System.getProperty("java.library.path"));
+
     this.mainDir = mainDir;
     Properties config = new Properties();
     FileInputStream in = null;
@@ -127,39 +128,40 @@ public class HandTrackingApp extends KeyAdapter {
       logger.severe(ioe.getMessage());
       System.exit(-1);
     }
-    
-    String openniConfigFile = mainDir + config.getProperty("openni-config", 
-        "config/config.xml");
-    
-    String saveFingertipProperty = config.getProperty("save-fingertip-data", 
+
+    String openniConfigFile = mainDir
+        + config.getProperty("openni-config", "config/config.xml");
+
+    String saveFingertipProperty = config.getProperty("save-fingertip-data",
         "false");
     if (saveFingertipProperty.equals("true"))
       saveFingertip = true;
-    
+
     String labelFile = config.getProperty("fingertip-label-file", null);
     if (labelFile != null)
       labelFile = mainDir + FINGERTIP_DIR + labelFile;
-    
+
     String displayOnProperty = config.getProperty("display-on", "true");
-    
+
     int maxDepth = EnvConstants.DEFAULT_MAX_DEPTH;
     try {
       maxDepth = Integer.parseInt(config.getProperty("max-depth", "1600"));
-    } catch(NumberFormatException efe) {
+    } catch (NumberFormatException efe) {
       System.err.println(efe.getMessage());
       System.err.println(String.format("maxDepth = %d", maxDepth));
     }
-    
-    String derivativeSaveDir = mainDir + config.getProperty("derivative-dir", 
-        "data/derivative/");
-    String calibrationFile = mainDir + config.getProperty("calibration-file",
-        "data/calibration/calibration.txt");
-    
+
+    String derivativeSaveDir = mainDir
+        + config.getProperty("derivative-dir", "data/derivative/");
+    String calibrationFile = mainDir
+        + config.getProperty("calibration-file",
+            "data/calibration/calibration.txt");
+
     if (displayOnProperty.equals("false"))
       displayOn = false;
-    
+
     try {
-      engine = new HandTrackingEngine(openniConfigFile, calibrationFile, 
+      engine = new HandTrackingEngine(openniConfigFile, calibrationFile,
           maxDepth);
     } catch (GeneralException ge) {
       logger.info("OpenNI config file = " + openniConfigFile);
@@ -168,44 +170,43 @@ public class HandTrackingApp extends KeyAdapter {
     }
     handEventListener = new HandEventListener();
     engine.addListener(handEventListener);
-    
+
     if (displayOn) {
       try {
         HashMap<Integer, List<Point>> labels = null;
         if (labelFile != null)
-          labels = (HashMap<Integer, List<Point>>) ObjectIO.readObject(
-              labelFile);
+          labels = (HashMap<Integer, List<Point>>) ObjectIO.readObject(labelFile);
 
-        packetController = new ProcessPacketController(engine.depthWidth(), 
+        packetController = new ProcessPacketController(engine.depthWidth(),
             engine.depthHeight(), labels);
-        
+
         packetController.addKeyListener(this);
         packetController.derivativeSaveDir = derivativeSaveDir;
         packetController.showUI();
-        
+
       } catch (IOException e) {
         System.err.println(e.getMessage());
         System.exit(-1);
       }
     }
-    
+
     while (isRunning()) {
       try {
         if (isPaused())
           continue;
         engine.step();
-        
-        if (packetController != null) 
+
+        if (packetController != null)
           packetController.show(engine.packet());
-        
-        if (engine.isTableInitialized() && tableFrame == null) {
+
+        if (displayOn && engine.isTableInitialized() && tableFrame == null) {
           tableFrame = new Table3DFrame(engine.table());
           Rectangle rect = packetController.getViewBounds();
           tableFrame.setLocation(rect.width, 0);
           tableFrame.addKeyListener(this);
           tableFrame.showUI();
         }
-        
+
         if (tableFrame != null)
           tableFrame.redraw(engine.packet());
       } catch (GeneralException ge) {
@@ -219,19 +220,19 @@ public class HandTrackingApp extends KeyAdapter {
     engine.release();
     System.exit(0);
   }
-  
+
   /**
    * Prints finger events for evaluation.
    */
   public void print() {
     if (!saveFingertip)
       return;
-    
+
     PrintWriter pw = null;
     try {
-      Date date =  new Date();
-      String fingertipFile = mainDir + FINGERTIP_DIR + dateFormat.format(date) + 
-          ".txt";
+      Date date = new Date();
+      String fingertipFile = mainDir + FINGERTIP_DIR + dateFormat.format(date)
+          + OUTPUT_EXTENSION;
       pw = new PrintWriter(fingertipFile);
       handEventListener.toOutput(pw);
       logger.info("Tracker controller output done.");
@@ -242,16 +243,15 @@ public class HandTrackingApp extends KeyAdapter {
         pw.close();
     }
   }
-  
+
   public boolean isRunning() {
-    return ((packetController != null && packetController.isVisible() || 
-        displayOn == false) && !engine.isDone());
+    return ((packetController != null && packetController.isVisible() || displayOn == false) && !engine.isDone());
   }
-  
+
   public boolean isPaused() {
     return packetController != null && paused;
   }
-    
+
   public void keyPressed(KeyEvent ke) {
     switch (ke.getKeyCode()) {
       case KeyEvent.VK_N:
