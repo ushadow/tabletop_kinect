88daadc09fb16aa7f4a5b858d257a386585d22b2
yingyin <clevershadow@gmail.com>
2013-01-02 14:57:05 -0500

    Added unit tests for the HandPoseDescriptor class.

--- src/edu/mit/yingyin/tabletop/models/HandPoseDescriptor.java
+++ src/edu/mit/yingyin/tabletop/models/HandPoseDescriptor.java
@@ -2,28 +2,43 @@ package edu.mit.yingyin.tabletop.models;
 
 import java.nio.FloatBuffer;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 import com.googlecode.javacv.cpp.opencv_core.CvMat;
 
 public class HandPoseDescriptor {
+  private static final Logger LOGGER = Logger.getLogger(
+      HandPoseDescriptor.class.getName());
   private final int NUM_CIRCLES = 5;
   private final int NUM_SECTORS = 8;
   private final int NUM_DEPTH_SECTIONS = 5;
   private final float radius, radiusWidthInv, sectorWidthInv, depthWidthInv;
   private float minDepth, maxDepth;
-  private float[][][] histogram = 
-      new float[NUM_CIRCLES][NUM_SECTORS][NUM_DEPTH_SECTIONS]; 
+  private float[] histogram = 
+      new float[NUM_CIRCLES * NUM_SECTORS * NUM_DEPTH_SECTIONS]; 
   
   public HandPoseDescriptor(CvMat points) {
+    if (points.cols() != 3)
+      throw new IllegalArgumentException(
+          "The points matrix should have 3 colums.");
     radius = findRadius(points);
     radiusWidthInv = NUM_CIRCLES / radius;
-    sectorWidthInv = (float) (NUM_SECTORS / Math.PI * 2);
+    sectorWidthInv = (float) (NUM_SECTORS / (Math.PI * 2));
     findDepthMinMax(points);
     depthWidthInv = NUM_DEPTH_SECTIONS / (maxDepth - minDepth);
     computeDescriptor(points);
   }
 
   public double radius() { return radius; }
+  
+  public float descriptorValue(int r, int s, int d) {
+    return histogram[r * NUM_SECTORS * NUM_DEPTH_SECTIONS + 
+                     s * NUM_DEPTH_SECTIONS + d];
+  }
+  
+  public String toString() {
+    return Arrays.toString(histogram);
+  }
   
   /**
    * 
@@ -55,26 +70,24 @@ public class HandPoseDescriptor {
     FloatBuffer fb = points.getFloatBuffer();
     fb.rewind();
     int total = 0;
-    Arrays.fill(histogram, 0);
     while (fb.remaining() > 0) {
       fb.get(p);
       float r = (float) Math.sqrt(p[0] * p[0] + p[1] * p[1]);
-      if (r < radius) {
+      if (r <= radius) {
         // From -pi to pi.
         float theta = (float) (Math.atan2(p[1], p[0]) + Math.PI);
         int rIndex = (int) (r * radiusWidthInv); 
+        rIndex = Math.min(rIndex, NUM_CIRCLES - 1);
         int sIndex = (int) (theta * sectorWidthInv);
+        sIndex = Math.min(sIndex, NUM_SECTORS - 1);
         int dIndex = (int) ((p[2] - minDepth) * depthWidthInv);
-        histogram[rIndex][sIndex][dIndex]++;
+        dIndex = Math.min(dIndex, NUM_DEPTH_SECTIONS - 1);
+        histogram[rIndex * NUM_SECTORS * NUM_DEPTH_SECTIONS + 
+                  sIndex * NUM_DEPTH_SECTIONS + dIndex]++;
         total++;
       }
     }
-    for (int r = 0; r < NUM_CIRCLES; r++) {
-      for (int s = 0; s < NUM_SECTORS; s++) {
-        for (int d = 0; d < NUM_DEPTH_SECTIONS; d++) {
-          histogram[r][s][d] /= total;
-        }
-      }
-    }
+    for (int i = 0; i < histogram.length; i++) 
+      histogram[i] /= total;
   }
 }
