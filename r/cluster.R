fe67d7f4d518b5a72f8e51506437fbbb5a16730c
yingyin <clevershadow@gmail.com>
2013-02-05 16:55:27 +0800

    Used saturation to reflect different values in the descriptor.

--- r/cluster.R
+++ r/cluster.R
@@ -17,21 +17,29 @@ PlotDescriptor <- function(descriptor) {
   kNumDepthDivs <- 5
   kNumSectors <- 8
   kNumRadDivs <- 5
+  descriptor <- unlist(descriptor)
+  max.value <- max(descriptor)
   m <- matrix(descriptor, kNumSectors * kNumRadDivs, kNumDepthDivs, TRUE)
-  palette <- heat.colors(kNumDepthDivs)
+  print(m)
+  palette <- rev(heat.colors(kNumDepthDivs))
   for (r in (kNumRadDivs - 1) : 0){
     edges <- c()
     colors <- c()
     extends <- list()
     for (s in 0 : (kNumSectors - 1)) {
       # Row index. 
-      rindex <- r * kNumRadDivs + s + 1
-      if (all(m[rindex,] == 0)) { colors <- c(colors, "#FFFFFFFF")
+      rindex <- r * kNumSectors + s + 1
+      if (all(m[rindex,] == 0)) { 
+        color <- "#FFFFFFFF"
       } else {
         # Depth index.
         dindex <- which.max(m[rindex,])
-        colors <- c(colors, palette[dindex])
+        color <- palette[dindex]
+        value <- m[rindex, dindex] / max.value
+        color <- ChangeHSV(color, value)
       }
+      print(color)
+      colors <- c(colors, color)
       extends <- c(extends, list(r : (r + 1))) 
       edges <- c(edges, -pi + pi * 2 * s / kNumSectors) 
     } 
@@ -40,11 +48,17 @@ PlotDescriptor <- function(descriptor) {
     if (names(dev.cur()) == "null device") {
       add = FALSE
     }
-    print(extends)
-    print(colors)
-    print(edges)
-    print(add)
     radial.pie(extends, sector.colors = colors, start = -pi, 
                show.grid.labels = FALSE, add = add)
   }
+}
+
+# Changes the alpha value of a color.
+#
+# Args:
+#   color: a R color. 
+#   alpha: between 0 - 255.
+ChangeHSV <- function(color, value) {
+  hsv.value <- rgb2hsv(col2rgb(color))
+  hsv(hsv.value[1], value, hsv.value[3])
 }
