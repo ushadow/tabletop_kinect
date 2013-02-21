365bfa1433dee21b4fb06c9b6b671ce1b3bb7e36
yingyin <clevershadow@gmail.com>
2013-02-21 18:41:05 -0500

    WIP: AHMM model.

---
+++ matlab/createmodel.m
@@ -0,0 +1,56 @@
+function ahmm = createmodel(nS, params)
+% CREATEMODEL creates a graphical AHMM model.
+% 
+% ahmm = createmodel(nS)
+% nS: number of hidden states for S.
+% params: a structure of parameters for different CPDs.
+%
+% Topology of the model:
+% G1->G2 
+% |\ / |\
+% | F1 | F2
+% |/   |/
+% S1->S2
+% |    |
+% X1  X2
+
+ss = 4; % Number of nodes in one time slice.
+nG = 5; % Select, manipulative, stop, point, come back.
+nX = 6;
+nF = 2; % 0, 1
+
+intra = zeros(ss);
+G1 = 1; S1 = 2; F1 = 3; X1 = 4; 
+G2 = G1 + ss; S2 = S2 + ss; F2 = F1; X2 = X1; 
+intra(G1, S1) = 1;
+intra(S1, X1) = 1;
+intra(G1, F1) = 1;
+intra(S1, F1) = 1;
+
+inter = zeros(ss);
+inter(G1, G1) = 1;
+inter(F1, G1) = 1;
+inter(S1, S1) = 1;
+ 
+nodeSizes = [nG nS nF nX];
+dnodes = [G1 S1 F1];
+onodes = X1;
+
+% eclass1(i) is the equivalence class that node i in slice 1 belongs to. 
+% eclass2(i) is the equivalence class that node i in slice 2, 3, ..., 
+% belongs to.
+eclass1 = 1 : ss;
+eclass2 = [G2, S2, F2, X2];
+
+ahmm = mk_dbn(intra, inter, nodeSizes, 'discrete', dnodes, 'observed', ...
+              onodes, 'eclass1', eclass1, 'eclass2', eclass2);
+
+% Set CPD.
+ahmm.CPD{G1} = tabular_CPD(ahmm, G1, params.G1);
+ahmm.CPD{S1} = tabular_CPD(ahmm, S1, params.S1);
+ahmm.CPD{F1} = hhmmF_CPD(ahmm, F1, S1, [], 'Qps', G1, 'termprob', ...
+                         params.F1);
+%ahmm.CPD{X1} = 
+ahmm.CPD{G2} = tabular_CPD(ahmm, G2, params.G2);
+ahmm.CPD{S2} = tabular_CPD(ahmm, S2, params.S2);
+end
---
+++ matlab/learn_ahmm_params.m
@@ -0,0 +1,13 @@
+function learn_ahmm_params(ahmm, data, max_iter)
+% LEARN_AHMM_PARAMS
+%
+% learn_ahmm_params(data)
+% ahmm: the model.
+% data: training data. A cell-array of 2D cell-arrays. For example,
+% case{l}{i, t} is the value of node i in slice t in sequel l, or [] if
+% unobserved.
+
+engine = smoother_engine(jtree_2TBN_inf_engine(ahmm));
+
+learn_params_dbn_em(engine, data, 'max_iter', max_iter);
+end
