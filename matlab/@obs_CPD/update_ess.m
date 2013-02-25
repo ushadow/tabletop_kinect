f75b3d76d4f6c8544c68c7152aef3d5851e37aeb
yingyin <clevershadow@gmail.com>
2013-02-25 18:53:03 -0500

    WIP: adding observation CPD.

--- matlab/@obs_CPD/update_ess.m
+++ matlab/@obs_CPD/update_ess.m
@@ -1,11 +1,23 @@
 function CPD = update_ess(CPD, fmarginal, evidence, ns, cnodes, ...
                           hiddent_bitv)
-% Figure out the node numbers associated with the parent.
+% fmarginal: the marginal on self's family which includes self's parents
+% and self.
+% evidence: 2D cell array for one sequence.
+                        
+% Figure out the node numbers associated with the parent. The node number
+% is unrolled in time, i.e. i + (t - 1) * ss.
 dom = fmarginal.domain;
 self = dom(end);
 
-% Discrete parent.
-dp = dom(1 : end - 1);
+CPD.nsamples = CPD.nsamples + 1;
+[ss dpsz] = size(CPD.mean); % ss = self size
 
-
+w = fmarginal.T(:);
+y = evidence{self};
+Cyy = y * y';
+WY = repmat(w(:)', ss, 1); % WY(y, i) = w(i)
+WYY = repmat(reshape(WY, [ss 1 dpsz]), [1 ss 1]); % WYY((y, y', i) = w(i)
+CPD.WYsum = CPD.WYsum + y(:) * w(:)'; % w(:)' is a row matrix.
+CPD.WYYsum = CPD.WYYsum + ...
+             WYY .* repmat(reshape(Cyy, [ss ss 1]), [1 1 dpsz]);
 end
